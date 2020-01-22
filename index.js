915ce951bdf1dd129f223cbec5c3bf30c046af1f
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2020-01-22 06:59:58 -0600

    quiz

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function click(elem) {
 	elem.classList.toggle("open");
 }
+function answer(txt){
+	document.getElementsByClassName("test-output")[0].innerHTML = txt;
+}
+
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function answer(text) {
-  document.getElementById('test-output').innerHTML = text;
+function click(elem) {
+	elem.classList.toggle("open");
+}
+function answer(txt){
+	document.getElementsByClassName("test-output")[0].innerHTML = txt;
 }
