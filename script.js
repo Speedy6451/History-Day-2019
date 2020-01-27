4570e3639c548555dea6304d577975a3bb0129c1
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2020-01-27 12:25:10 -0600

    modals r hard

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function click(elem) {
-  console.log("l");
+function loadImgs() {
+	for (i in document.getElementsByClassName("open")) {
+		//i.
+	}
+}
+function clik(elem) {
+	console.log("l");
 	elem.classList.toggle("open");
 }
 function answer(txt){
 	document.getElementsByClassName("test-output")[0].innerHTML = txt;
+	console.log("log")
 }
+console.log("log");
